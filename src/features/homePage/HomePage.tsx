6a72155c68b60478cab0fb0e7bba46b851300c6e
Socolov Alexandr <82725768+assassin0210@users.noreply.github.com>
2023-05-29 19:08:46 +0300

    fix menu

--- src/features/homePage/HomePage.tsx
+++ src/features/homePage/HomePage.tsx
@@ -7,7 +7,7 @@ export const HomePage = () => {
   return (
     <main className=" w-full hr:gap-10 pt-20">
       {blocks.map(({ id, Component }) => (
-        <Element key={id} name={id} id={id}>
+        <Element key={id} name={id} id={id} className="content-block">
           <Component />
         </Element>
       ))}

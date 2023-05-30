b518b427ee97b727da98860acfcb768f78ca9fe5
Socolov Alexandr <82725768+assassin0210@users.noreply.github.com>
2023-05-30 20:32:47 +0300

    fix

--- src/features/mouseEffect/MouseEffect.tsx
+++ src/features/mouseEffect/MouseEffect.tsx
@@ -6,8 +6,7 @@ import { useListenerTheme } from '@/widgets/providers/theme/useListenerTheme'
 export const MouseEffect = ({ children }: { children: ReactNode }) => {
   const [position, setPosition] = useState({ x: '0px', y: '0px' })
   const theme = useListenerTheme()
-  console.log(theme)
-  // console.log(theme)
+
   const style = useMemo(
     () =>
       `radial-gradient(600px at ${position.x} ${position.y}, rgba(29, 78, 216, 0.15), transparent 80%)`,
